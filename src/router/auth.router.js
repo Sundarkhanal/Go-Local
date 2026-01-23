bd5facea037e947740afadb820c4afdfb40bd9c6
Sundar Khanal <sundarkhanal62@gmail.com>
2026-01-23 00:08:22 -0500

    validation rules added using Joi

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -2,7 +2,7 @@ const authRouter = require("express").Router()
 const authCtrl = require("../controller/auth.controller")
 const uploader = require("../middleware/uploader.middleware")
 
-authRouter.post('/register', uploader().single(), authCtrl.register )
+authRouter.post('/register', uploader().single("image"), authCtrl.register )
 authRouter.get('/activate-user', authCtrl.activateUser)
 authRouter.post('/login', authCtrl.login)
 authRouter.get('/me', authCtrl.getLoggedInUser)
---
+++ src/rules/auth.rule.js
@@ -0,0 +1,16 @@
+const Joi = require("joi");
+const { join } = require("node:path");
+
+
+
+const RegisterDTO = Joi.object({
+    name: Joi.string().min(2).max(30).required().messages(),
+    email:Joi.string().email().required(),
+    phone:Joi.string().required().min(10).max(21).optional().allow(null, '').default(null),
+    password:Joi.string().min(8).max(15).required(),
+    confirmPassword:Joi.ref('password'),
+    role: Joi.string().allow("customer", "admin").default("customer"),
+    address:Joi.string(),
+    gender:Joi.string().allow("male", "female", "others").required(),
+    image:Joi.string()
+})
