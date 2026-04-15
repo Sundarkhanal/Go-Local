853764a3e396dcf284ba8a5b948de54a82b168b8
Sundar Khanal <sundarkhanal62@gmail.com>
2026-04-15 09:47:57 -0400

    setup react

--- server/router/esewa.payment.router.js
+++ server/router/esewa.payment.router.js
@@ -1,6 +1,6 @@
 const epaymentRouter = require("express").Router()
-const checkPermission = require("../../server/middleware/auth.middleware")
-const paymentCtrl = require("../../server/controller/esewa.payment.controller")
+const checkPermission = require("../middleware/auth.middleware")
+const paymentCtrl = require("../controller/esewa.payment.controller")
 
 epaymentRouter.post('/initiate', checkPermission(), paymentCtrl.initiatePayment)
 epaymentRouter.get('/success', checkPermission(), paymentCtrl.paymentSuccess)
