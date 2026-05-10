ecee6dd5d071d1f24fc781d28b0a927598ede250
Sundar Khanal <sundarkhanal62@gmail.com>
2026-05-10 12:00:24 -0400

    fix: update socket URL for production

--- client/src/lib/socket/socket.ts
+++ client/src/lib/socket/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:9005";
 
-export const socket = io("http://localhost:9005", {
+export const socket = io(SOCKET_URL, {
   autoConnect:false,
   withCredentials: true,
 });
